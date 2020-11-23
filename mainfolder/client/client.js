a9315e0a47b47a1dd58d1a4ae77b0b4ff3261667
peppe31 <74790176+peppe31@users.noreply.github.com>
2020-11-23 20:26:04 +0100

    23

--- mainfolder/client/client.js
+++ mainfolder/client/client.js
@@ -1,13 +1,11 @@
 console.log("Leggi le news !");
 
-const newsElement = document.querySelector(".news");
+const newsElement = document.querySelector("aggiungiNews");
 const form = document.querySelector("form"); // grabbing an element on the page
 const errorElement = document.querySelector(".error-message");
-const loadingElement = document.querySelector(".loading");
+
 const loadMoreElement = document.querySelector("#loadMore");
-const API_URL =
-  window.location.hostname === "127.0.0.1" ||
-  window.location.hostname === "localhost";
+const getNewsAPI_URL = "http://localhost:5000/news";
 
 let skip = 0;
 let limit = 5;
@@ -25,56 +23,45 @@ listAllNews();
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  console.log("Notizia aggiunta !");
   const formData = new FormData(form);
-  const name = formData.get("name");
-  const content = formData.get("content");
+  const titolo = formData.get("titolo");
+  const testo = formData.get("testo");
+  console.log(titolo + " " + testo);
+  const comunicazione = {
+    titolo,
+    testo,
+  };
 
-  if (name.trim() && content.trim()) {
-    errorElement.style.display = "none";
-    form.style.display = "none";
-    loadingElement.style.display = "";
-
-    const news = {
-      name,
-      content,
-    };
-
-    fetch(API_URL, {
-      method: "POST",
-      body: JSON.stringify(news),
-      headers: {
-        "content-type": "application/json",
-      },
-    })
-      .then((response) => {
-        if (!response.ok) {
-          const contentType = response.headers.get("content-type");
-          if (contentType.includes("json")) {
-            return response
-              .json()
-              .then((error) => Promise.reject(error.message));
-          } else {
-            return response.text().then((message) => Promise.reject(message));
-          }
+  fetch(getNewsAPI_URL, {
+    method: "POST",
+    body: JSON.stringify(comunicazione),
+    headers: {
+      "content-type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (!response.ok) {
+        const contentType = response.headers.get("content-type");
+        if (contentType.includes("json")) {
+          return response.json().then((error) => Promise.reject(error.message));
+        } else {
+          return response.text().then((message) => Promise.reject(message));
         }
-      })
-      .then(() => {
-        form.reset();
-        setTimeout(() => {
-          form.style.display = "";
-        }, 30000);
-        listAllNews();
-      })
-      .catch((errorMessage) => {
+      }
+    })
+    .then(() => {
+      form.reset();
+      setTimeout(() => {
         form.style.display = "";
-        errorElement.textContent = errorMessage;
-        errorElement.style.display = "";
-        loadingElement.style.display = "none";
-      });
-  } else {
-    errorElement.textContent = "Name and content are required!";
-    errorElement.style.display = "";
-  }
+      }, 2000);
+    });
+  /*.catch((errorMessage) => {
+      form.style.display = "";
+      errorElement.textContent = errorMessage;
+      errorElement.style.display = "";
+      loadingElement.style.display = "none";
+    });*/
 });
 
 function loadMore() {
@@ -88,20 +75,21 @@ function listAllNews(reset = true) {
     skip = 0;
     finished = false;
   }
-  fetch(`${API_URL}?skip=${skip}&limit=${limit}`)
+  fetch(getNewsAPI_URL) //`${}?skip=${skip}&limit=${limit}`
     .then((response) => response.json())
     .then((result) => {
-      result.news.forEach((news) => {
+      console.log(result.news);
+      result.news.forEach((comunicazione) => {
         const div = document.createElement("div");
 
         const header = document.createElement("h3");
-        header.textContent = news.name;
+        header.textContent = comunicazione.name;
 
         const contents = document.createElement("p");
-        contents.textContent = news.content;
+        contents.textContent = comunicazione.content;
 
         const date = document.createElement("small");
-        date.textContent = new Date(news.created);
+        date.textContent = new Date(comunicazione.data);
 
         div.appendChild(header);
         div.appendChild(contents);
---
+++ mainfolder/index.js
@@ -0,0 +1,116 @@
+const Comunicazione = require("./src/Comunicazione");
+const express = require("express");
+const cors = require("cors");
+const monk = require("monk");
+//const Filter = require("bad-words");
+
+const app = express();
+
+const db = monk(process.env.MONGO_URI || "localhost:27017");
+const newsFeed = db.get("HAP-comunicazioni");
+//const filter = new Filter();
+
+app.enable("trust proxy");
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("Listening on http://localhost:5000");
+});
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Benvenuto !",
+  });
+});
+
+app.get("/news", (req, res, next) => {
+  newsFeed
+    .find()
+    .then((news) => {
+      console.log(news);
+      res.json(news);
+    })
+    .catch(next);
+});
+
+/*app.get("/v2/news", (req, res, next) => {
+  // let skip = Number(req.query.skip) || 0;
+  // let limit = Number(req.query.limit) || 10;
+  let { skip = 0, limit = 5, sort = "desc" } = req.query;
+  skip = parseInt(skip) || 0;
+  limit = parseInt(limit) || 5;
+
+  skip = skip < 0 ? 0 : skip;
+  limit = Math.min(50, Math.max(1, limit));
+
+  Promise.all([
+    news.count(),
+    news.find(
+      {},
+      {
+        skip,
+        limit,
+        sort: {
+          created: sort === "desc" ? -1 : 1,
+        },
+      }
+    ),
+  ])
+    .then(([total, news]) => {
+      res.json({
+        news,
+        meta: {
+          total,
+          skip,
+          limit,
+          has_more: total - (skip + limit) > 0,
+        },
+      });
+    })
+    .catch(next);
+});
+
+function isValidNews(news) {
+  return (
+    news.name &&
+    news.name.toString().trim() !== "" &&
+    news.name.toString().trim().length <= 50 &&
+    news.content &&
+    news.content.toString().trim() !== "" &&
+    news.content.toString().trim().length <= 140
+  );
+}*/
+
+const createNews = (req, res, next) => {
+  //if (isValidNews(req.body)) {
+  const comunicazione = {
+    titolo: req.body.titolo.toString().trim(),
+    testo: req.body.testo.toString().trim(),
+    data: new Date(),
+  };
+  newsFeed
+    .insert(comunicazione)
+    .then((createdNews) => {
+      res.json(createdNews);
+    })
+    .catch(next);
+  /*} else {
+    res.status(422);
+    res.json({
+      message:
+        "Hey! Name and Content are required! Name cannot be longer than 50 characters. Content cannot be longer than 140 characters.",
+    });
+  }*/
+};
+
+app.post("/news", createNews);
+//app.post("/v2/news", createNews);
+
+app.use((error, req, res, next) => {
+  res.status(500);
+  res.json({
+    message: error.message,
+  });
+});
