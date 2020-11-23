a22dc612e2c98675e87d4e13e519c60035b93cb0
Phil <filippo.troiani96@gmail.com>
2020-11-23 20:30:19 +0100

    poca roba

--- mainfolder/client/Prenota/prenota.js
+++ mainfolder/client/Prenota/prenota.js
@@ -1,4 +1,5 @@
 import Data from "../../src/Data.js";
+const postPrenotazioneAPI_URL = "http://localhost:5000/prenota";
 
 const orariDisponibiliContainerElement = document.getElementById(
   "orariDisponibiliContainer"
@@ -19,6 +20,7 @@ const initPage = () => {
   const today = new Data();
   prenotaDateElement.setAttribute("min", today.getFullDataHTML());
   prenotaDateElement.value = today.getFullDataHTML();
+  caricaOrariDisponibili(today);
 
   //aggiungo gli event listeners
   formElement.addEventListener("submit", (event) => {
@@ -30,8 +32,6 @@ const initPage = () => {
     data.setDataFromString(prenotaDateElement.value);
     caricaOrariDisponibili(data); //decidere in quale formato passare la data
   });
-
-  prenotaDateElement.value = today.getFullDataHTML();
 };
 
 const submitProcedure = () => {
@@ -45,16 +45,29 @@ const submitProcedure = () => {
 
 const caricaOrariDisponibili = (data) => {
   const orari = acquisisciOrariDisponibili(data);
+  console.log(orari);
   const orarioDisponibileElement = document.createElement("div");
-  orari.forEach((element) => {
-    orarioDisponibileElement.innerHTML = element;
+  orari.forEach((item) => {
+    //element
+    orarioDisponibileElement.innerHTML = item;
     orariDisponibiliContainerElement.appendChild(orarioDisponibileElement);
   });
 };
 
 const acquisisciOrariDisponibili = (data) => {
   //get request
+  const orari = testOrari();
+  return orari;
+};
+
+const testOrari = () => {
   const orari = [];
-  orari[0] = data.getFullData();
+  const minuti = 1000 * 60;
+  for (var i = 0; i < 18; i++) {
+    const ora = new Date(8 * 60 * minuti + i * 20 * minuti);
+
+    orari[i] = `${ora.getHours()}:${ora.getMinutes()}`;
+  }
+
   return orari;
 };
